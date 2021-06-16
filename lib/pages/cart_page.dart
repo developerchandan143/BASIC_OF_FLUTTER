23e9c109e24b8ef320eb77e7b38dd46e737433de
developerchandan143 <vishachandan93@gmail.com>
2021-06-16 12:48:57 +0530

    Master course

---
+++ lib/pages/cart_page.dart
@@ -0,0 +1,87 @@
+import 'package:basic_of_flutter/modols/cart.dart';
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'package:basic_of_flutter/modols/catelog.dart';
+
+class CartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: context.canvasColor,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        title: "Cart".text.make(),
+      ),
+      body: Column(
+        children: [
+          _CartList().p32().expand(),
+          Divider(),
+          _CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 200,
+      child: SingleChildScrollView(
+        padding: EdgeInsets.zero,
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            "\$9999".text.xl5.color(context.theme.accentColor).make(),
+            30.widthBox,
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: "Buying not supported yet.".text.make(),
+                  ),
+                );
+              },
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all(context.theme.buttonColor),
+              ),
+              child: "Buy".text.make(),
+            ).w32(context),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  @override
+  __CartListState createState() => __CartListState();
+}
+
+class __CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    final _cart = CartModel();
+
+    return ListView.builder(
+      itemCount: _cart.items?.length,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.done),
+        trailing: IconButton(
+          icon: Icon(Icons.remove_circle_outline),
+          onPressed: () {},
+        ),
+        title: "item".text.make(),
+      ),
+    );
+  }
+}
+
+class CortModel {
+  get items => null;
+}
